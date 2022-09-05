1ef71e13e2c8cc61c757cc8b9da997ac32b0c98e
kstppd <kpapadakis@protonmail.com>
2022-09-05 20:37:56 +0300

    Add&Delete test updates

--- tests/add_remove_map_elements/main.cu
+++ tests/add_remove_map_elements/main.cu
@@ -3,9 +3,16 @@
 #include <chrono>
 #include "../../src/hashinator/hashinator.h"
 #include <gtest/gtest.h>
-#define N  32
+#define N 1<<17
 
 typedef uint32_t val_type;
+
+
+void cpu_write_map(Hashinator<val_type,val_type>& map,int total_keys=N){
+   for (val_type i=0; i<total_keys;i++){
+      map[i]=0;
+   }
+}
 
 __global__
 void gpu_write_map(Hashinator<val_type,val_type> *dmap){
@@ -16,11 +23,13 @@ void gpu_write_map(Hashinator<val_type,val_type> *dmap){
    return;
 }
 
+
 __global__
-void gpu_delete_even(Hashinator<val_type,val_type> *dmap){
+void gpu_delete_all(Hashinator<val_type,val_type> *dmap){
    int index = blockIdx.x * blockDim.x + threadIdx.x;
-   if (index<N && index%2==0){
-      dmap->d_erase(index);
+   if (index<N ){
+      auto kpos=dmap->d_find(index);
+      dmap->d_erase(kpos);
    }
    return;
 }
@@ -28,10 +37,14 @@ void gpu_delete_even(Hashinator<val_type,val_type> *dmap){
 
 
 __global__
-void gpu_delete_all(Hashinator<val_type,val_type> *dmap){
+void gpu_delete_even(Hashinator<val_type,val_type> *dmap){
    int index = blockIdx.x * blockDim.x + threadIdx.x;
    if (index<N ){
-      dmap->d_erase(index);
+      auto kpos=dmap->d_find(index);
+      if (kpos==dmap->d_end()){return;}
+      if (kpos->second %2==0 || kpos->second==0){
+         dmap->d_erase(kpos);
+      }
    }
    return;
 }
@@ -40,42 +53,39 @@ void gpu_delete_all(Hashinator<val_type,val_type> *dmap){
 __global__
 void gpu_delete_odd(Hashinator<val_type,val_type> *dmap){
    int index = blockIdx.x * blockDim.x + threadIdx.x;
-   if (index<N && index%2==1){
-      dmap->d_erase(index);
+   if (index<N ){
+      auto kpos=dmap->d_find(index);
+      if (kpos==dmap->d_end()){return;}
+      if (kpos->second %2==1){
+         dmap->d_erase(kpos);
+      }
    }
    return;
 }
-
-void cpu_write_map(Hashinator<val_type,val_type>& map,int total_keys=N){
-   for (val_type i=0; i<total_keys;i++){
-      map[i]=0;
-   }
-}
-
-
 
 
 int main(int argc, char* argv[]){
    Hashinator<val_type,val_type> hmap;
    cpu_write_map(hmap,N);
    size_t threads=32;
-   size_t blocks=1024;
+   size_t blocks=1<<26;
 
-   Hashinator<val_type,val_type>* dmap=hmap.upload();
+   Hashinator<val_type,val_type>* dmap;
+   dmap=hmap.upload();
    gpu_write_map<<<blocks,threads>>> (dmap);
    cudaDeviceSynchronize();
    hmap.clean_up_after_device(dmap);
 
-
-   Hashinator<val_type,val_type>* dmap2=hmap.upload();
-   gpu_delete_even<<<blocks,threads>>> (dmap2);
+   dmap=hmap.upload();
+   gpu_delete_even<<<blocks,threads>>> (dmap);
+   //gpu_delete_even<<<N,1>>> (dmap);
    cudaDeviceSynchronize();
-   hmap.clean_up_after_device(dmap2);
+   hmap.clean_up_after_device(dmap);
 
-   Hashinator<val_type,val_type>* dmap3=hmap.upload();
-   gpu_delete_odd<<<blocks,threads>>> (dmap3);
+   dmap=hmap.upload();
+   gpu_delete_odd<<<blocks,threads>>> (dmap);
    cudaDeviceSynchronize();
-   hmap.clean_up_after_device(dmap3);
+   hmap.clean_up_after_device(dmap);
 
 
 
