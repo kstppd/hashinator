b0ecbbf872fcded86cb95837804f83cedcfa68db
kstppd <kpapadakis@protonmail.com>
2023-01-12 13:35:12 +0200

    A few more updates to benchmarks

--- benchmark/hashmap.cu
+++ benchmark/hashmap.cu
@@ -17,6 +17,16 @@ void create_input(vector& src){
    }
 }
 
+void extend_input(vector& src,size_t N){
+
+   auto offset=src.size();
+   src.resize(src.size()+N);
+   for (uint32_t i=0; i<N; ++i){
+      src[i+offset] = hash_pair<val_type,val_type>{(val_type)offset+i,(val_type)offset+i};
+   }
+
+}
+
 
 void hashmap_benchmark()
 {
@@ -30,8 +40,44 @@ void hashmap_benchmark()
    std::cout<<hmap.load_factor()<<std::endl;
 }
 
+
+
+bool recover_elements(const hashmap& hmap, vector& src){
+   for (size_t i=0; i<src.size(); ++i){
+      const hash_pair<val_type,val_type>& kval=src.at(i);
+      auto retval=hmap.find(kval.first);
+      if (retval==hmap.end()){assert(0&& "END FOUND");}
+      bool sane=retval->first==kval.first  &&  retval->second== kval.second ;
+      if (!sane){ 
+         return false; 
+      }
+   }
+   return true;
+}
+void hashmap_benchmark_lf()
+{
+   int power=22;
+   int step=1<<18;
+   hashmap hmap;
+   size_t N = 1<<power;
+   vector src(N);
+   create_input(src);
+   do{
+      hmap.clear();
+      src.optimizeGPU();
+      hmap.insert(src.data(),src.size(),power);
+      bool success=recover_elements(hmap,src);
+      if (!success){assert(false && "Map is illformed");}
+      std::cout<<"Load factor= "<<hmap.load_factor()<<std::endl;
+      extend_input(src,step);
+   }while(hmap.load_factor()<0.95);
+}
+
+
+
+
 int main()
 {
-   hashmap_benchmark();
+   hashmap_benchmark_lf();
    return 0;
 }
