4176c132670c7d1c805ef00ebfc1feb4b4cd44cd
kstppd <kpapadakis@protonmail.com>
2023-08-12 11:15:23 +0300

    Update test benchmark to use device only memory

--- tests/benchmark/main.cu
+++ tests/benchmark/main.cu
@@ -3,52 +3,49 @@
 #include <chrono>
 #include <random>
 #include "../../include/hashinator/hashinator.h"
-#include <gtest/gtest.h>
-
-#define BLOCKSIZE 32
-#define expect_true EXPECT_TRUE
-#define expect_false EXPECT_FALSE
-#define expect_eq EXPECT_EQ
-constexpr int MINPOWER = 5;
-constexpr int MAXPOWER = 20;
+constexpr int N = 24;
+constexpr int R = 10;
 
 using namespace std::chrono;
 using namespace Hashinator;
 typedef uint32_t val_type;
 typedef uint32_t key_type;
 typedef split::SplitVector<hash_pair<key_type,val_type>,split::split_unified_allocator<hash_pair<val_type,val_type>>,split::split_unified_allocator<size_t>> vector ;
+using hashmap= Hashmap<key_type,val_type>;
 
 
-
-void create_input(vector& src){
-   for (size_t i=0; i<src.size(); ++i){
-      hash_pair<key_type,val_type>& kval=src.at(i);
-      kval.first=i;
-      kval.second=i;
+void create_input(hash_pair<key_type,val_type>* src, int sz){
+   for (size_t i=0; i<(1<<sz); ++i){
+      src[i].first=i;
+      src[i].second=i;
    }
 }
 
-
-bool benchInsert(){
-   using hashmap= Hashmap<key_type,val_type>;
-   const int sz=24;
-   vector src(1<<sz);
-   create_input(src);
-   hashmap hmap(sz+1);
-   src.optimizeGPU();
-   hmap.optimizeGPU();
-   hmap.insert(src.data(),src.size());
-   return(hmap.peek_status()==status::success);
-}
-
-TEST(HashmapUnitTets ,Test_Copy_Metadata){
-   for (int i =0; i<10; i++){
-      expect_true(benchInsert());
-   }
+void benchInsert(hashmap& hmap,hash_pair<key_type,val_type>* src,int sz){
+   hmap.insert(src,1<<sz);
+   assert(hmap.peek_status()==status::success);
+   hmap.stats();
+   hmap.clear();
+   return ;
 }
 
 int main(int argc, char* argv[]){
-   srand(time(NULL));
-   ::testing::InitGoogleTest(&argc, argv);
-   return RUN_ALL_TESTS();
+
+   const int sz=N;
+   hashmap hmap(sz+1);
+   hmap.optimizeGPU();
+   vector cpu_src(1<<sz);
+   create_input(cpu_src.data(),sz);
+   hash_pair<key_type,val_type>* src;
+   cudaMalloc((void **) &src, (1<<sz)*sizeof(hash_pair<key_type,val_type>));
+   cudaMemcpy(src,cpu_src.data(),cpu_src.size()*sizeof(hash_pair<key_type,val_type>),cudaMemcpyHostToDevice);
+
+   for (int i =0; i<R; i++){
+      hmap.optimizeGPU();
+      benchInsert(hmap,src,sz);
+   }
+
+   cudaFree(src);
+   return 0;
+
 }
