91274a8ba738ddae7d2264d4e9bb56d1169bc051
kstppd <kpapadakis@protonmail.com>
2023-08-28 13:08:31 +0300

    More benches

--- unit_tests/benchmark/loadFactor.cu
+++ unit_tests/benchmark/loadFactor.cu
@@ -1,29 +1,73 @@
 #include <iostream>
 #include <stdlib.h>
 #include <chrono>
+#include <unordered_set>
 #include <random>
 #include "../../include/hashinator/hashinator.h"
-constexpr int R = 10;
 
+constexpr int R = 2;
 using namespace std::chrono;
 using namespace Hashinator;
 typedef uint32_t val_type;
 typedef uint32_t key_type;
 typedef split::SplitVector<hash_pair<key_type,val_type>,split::split_unified_allocator<hash_pair<val_type,val_type>>,split::split_unified_allocator<size_t>> vector ;
+typedef split::SplitVector<key_type,split::split_unified_allocator<key_type>,split::split_unified_allocator<size_t>> key_vec;
+typedef split::SplitVector<val_type,split::split_unified_allocator<val_type>,split::split_unified_allocator<size_t>> val_vec;
 using hashmap= Hashmap<key_type,val_type>;
 
+auto generateNonDuplicatePairs(vector& src,const size_t size)->void {
+    std::unordered_set<int> keys;
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<val_type> dist(1, std::numeric_limits<val_type>::max());
 
-void create_input(hash_pair<key_type,val_type>* src, size_t N){
-   for (size_t i=0; i<N; ++i){
-      src[i].first=i;
-      src[i].second=i;
-   }
+    src.clear();
+    while (src.size() < size) {
+        val_type key = dist(gen);
+        // Check if the key is already present
+        if (keys.find(key) == keys.end()) {
+           val_type val=static_cast<val_type>(key/2);
+            src.push_back({key,val});
+            keys.insert(key);
+        }
+    }
 }
 
-void benchInsert(hashmap& hmap,hash_pair<key_type,val_type>* src,vector& buffer ,size_t N){
-   hmap.insert(src,N,1);
-   hmap.retrieve(buffer.data(),N);
-   assert(hmap.peek_status()==status::success);
+auto generateNonDuplicatePairs(key_vec &keys,val_vec& vals,const size_t size)->void {
+    std::unordered_set<int> unique_keys;
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<val_type> dist(1, std::numeric_limits<val_type>::max());
+    keys.clear();
+    vals.clear();
+    while (keys.size() < size) {
+        val_type key = dist(gen);
+        // Check if the key is already present
+        if (unique_keys.find(key) == unique_keys.end()) {
+           val_type val=static_cast<val_type>(key/2);
+            keys.push_back(key);
+            vals.push_back(val);
+            unique_keys.insert(key);
+        }
+    }
+}
+
+template <class Fn, class ... Args>
+auto timeMe(Fn fn, Args && ... args){
+   std::chrono::time_point<std::chrono::_V2::system_clock, std::chrono::_V2::system_clock::duration> start,stop;
+   double total_time=0;
+   start = std::chrono::high_resolution_clock::now();
+   fn(args...);
+   stop = std::chrono::high_resolution_clock::now();
+   auto duration = duration_cast<milliseconds>(stop- start).count();
+   total_time+=duration;
+   return total_time;
+}
+
+
+void benchInsert(hashmap& hmap,key_type* gpuKeys, val_type* gpuVals,int size){
+   hmap.insert(gpuKeys,gpuVals,size,1);
+   hmap.retrieve(gpuKeys,gpuVals,size);
    hmap.stats();
    hmap.clear();
    return ;
@@ -33,26 +77,32 @@ int main(int argc, char* argv[]){
 
    const int sz=24;
    float targetLF=0.5;
-   if (argc>2){
+   if (argc>=2){
       targetLF=atof(argv[1]);
    }
    const size_t N = (1<<(sz+1))*targetLF;
-   std::cout<<targetLF<< " "<<N<<std::endl;
    hashmap hmap(sz+1);
    hmap.optimizeGPU();
-   vector cpu_src(N);
-   create_input(cpu_src.data(),N);
-   hash_pair<key_type,val_type>* src;
-   split_gpuMalloc((void **) &src, N*sizeof(hash_pair<key_type,val_type>));
-   split_gpuMemcpy(src,cpu_src.data(),cpu_src.size()*sizeof(hash_pair<key_type,val_type>),split_gpuMemcpyHostToDevice);
+   vector cpu_src;
+   key_vec cpu_keys;
+   val_vec cpu_vals;
+   std::cout<<targetLF<< " "<<N<<std::endl;
+   generateNonDuplicatePairs(cpu_keys,cpu_vals,N);
+   std::cout<<"Generated "<<cpu_keys.size()<<" unique keys!"<<std::endl;
 
+   key_type* gpuKeys;
+   val_type* gpuVals;
+   split_gpuMalloc((void **) &gpuKeys, (N)*sizeof(key_type));
+   split_gpuMalloc((void **) &gpuVals, (N)*sizeof(val_type));
+   split_gpuMemcpy(gpuKeys,cpu_keys.data(),(N)*sizeof(key_type),split_gpuMemcpyHostToDevice);
+   split_gpuMemcpy(gpuVals,cpu_vals.data(),(N)*sizeof(key_type),split_gpuMemcpyHostToDevice);
+
+
+   double t={0};
    for (int i =0; i<R; i++){
       hmap.optimizeGPU();
-      cpu_src.optimizeGPU();
-      benchInsert(hmap,src,cpu_src,N);
+      t+=timeMe(benchInsert,hmap,gpuKeys,gpuVals,N);
    }
-
-   split_gpuFree(src);
+   std::cout<<"Done in "<<t/R<<" ms"<<std::endl;
    return 0;
-
 }
--- unit_tests/benchmark/main.cu
+++ unit_tests/benchmark/main.cu
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <stdlib.h>
 #include <chrono>
+#include <unordered_set>
 #include <random>
 #include "../../include/hashinator/hashinator.h"
 constexpr int R = 10;
@@ -10,20 +11,63 @@ using namespace Hashinator;
 typedef uint32_t val_type;
 typedef uint32_t key_type;
 typedef split::SplitVector<hash_pair<key_type,val_type>,split::split_unified_allocator<hash_pair<val_type,val_type>>,split::split_unified_allocator<size_t>> vector ;
+typedef split::SplitVector<key_type> key_vec;
+typedef split::SplitVector<val_type> val_vec;
 using hashmap= Hashmap<key_type,val_type>;
 
 
-void create_input(hash_pair<key_type,val_type>* src, int sz){
-   for (size_t i=0; i<(1<<sz); ++i){
-      src[i].first=i;
-      src[i].second=i;
-   }
+
+auto generateNonDuplicatePairs(vector& src,const size_t size)->void {
+    std::unordered_set<int> keys;
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<val_type> dist(1, std::numeric_limits<val_type>::max());
+
+    src.clear();
+    while (src.size() < size) {
+        val_type key = dist(gen);
+        // Check if the key is already present
+        if (keys.find(key) == keys.end()) {
+           val_type val=static_cast<val_type>(key/2);
+            src.push_back({key,val});
+            keys.insert(key);
+        }
+    }
 }
 
-void benchInsert(hashmap& hmap,hash_pair<key_type,val_type>* src,vector& buffer ,int sz){
-   hmap.insert(src,1<<sz);
-   hmap.retrieve(buffer.data(),1<<sz);
-   assert(hmap.peek_status()==status::success);
+auto generateNonDuplicatePairs(key_vec &keys,val_vec& vals,const size_t size)->void {
+    std::unordered_set<int> unique_keys;
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<val_type> dist(1, std::numeric_limits<val_type>::max());
+    keys.clear();
+    vals.clear();
+    while (keys.size() < size) {
+        val_type key = dist(gen);
+        // Check if the key is already present
+        if (unique_keys.find(key) == unique_keys.end()) {
+           val_type val=static_cast<val_type>(key/2);
+            keys.push_back(key);
+            vals.push_back(val);
+            unique_keys.insert(key);
+        }
+    }
+}
+
+template <class Fn, class ... Args>
+auto timeMe(Fn fn, Args && ... args){
+   std::chrono::time_point<std::chrono::_V2::system_clock, std::chrono::_V2::system_clock::duration> start,stop;
+   double total_time=0;
+   start = std::chrono::high_resolution_clock::now();
+   fn(args...);
+   stop = std::chrono::high_resolution_clock::now();
+   auto duration = duration_cast<milliseconds>(stop- start).count();
+   total_time+=duration;
+   return total_time;
+}
+
+void benchInsert(hashmap& hmap,key_type* gpuKeys, val_type* gpuVals,int sz){
+   hmap.insert(gpuKeys,gpuVals,1<<sz,1);
    hmap.stats();
    hmap.clear();
    return ;
@@ -32,24 +76,33 @@ void benchInsert(hashmap& hmap,hash_pair<key_type,val_type>* src,vector& buffer 
 int main(int argc, char* argv[]){
 
    int sz= 24;
-   if (argc>2){
+   if (argc>=2){
       sz=atoi(argv[1]);
    }
    hashmap hmap(sz+1);
    hmap.optimizeGPU();
-   vector cpu_src(1<<sz);
-   create_input(cpu_src.data(),sz);
-   hash_pair<key_type,val_type>* src;
-   split_gpuMalloc((void **) &src, (1<<sz)*sizeof(hash_pair<key_type,val_type>));
-   split_gpuMemcpy(src,cpu_src.data(),cpu_src.size()*sizeof(hash_pair<key_type,val_type>),split_gpuMemcpyHostToDevice);
+   vector cpu_src;
+   key_vec cpu_keys;
+   val_vec cpu_vals;
+   generateNonDuplicatePairs(cpu_keys,cpu_vals,1<<sz);
+   std::cout<<"Generated "<<cpu_keys.size()<<" unique keys!"<<std::endl;
 
+   key_type* gpuKeys;
+   val_type* gpuVals;
+   split_gpuMalloc((void **) &gpuKeys, (1<<sz)*sizeof(key_type));
+   split_gpuMalloc((void **) &gpuVals, (1<<sz)*sizeof(val_type));
+   split_gpuMemcpy(gpuKeys,cpu_keys.data(),(1<<sz)*sizeof(key_type),split_gpuMemcpyHostToDevice);
+   split_gpuMemcpy(gpuVals,cpu_vals.data(),(1<<sz)*sizeof(key_type),split_gpuMemcpyHostToDevice);
+
+   double t={0};
    for (int i =0; i<R; i++){
       hmap.optimizeGPU();
-      cpu_src.optimizeGPU();
-      benchInsert(hmap,src,cpu_src,sz);
+      t+=timeMe(benchInsert,hmap,gpuKeys,gpuVals,sz);
    }
+   std::cout<<"Done in "<<t/R<<" ms"<<std::endl;
 
-   split_gpuFree(src);
+   split_gpuFree(gpuKeys);
+   split_gpuFree(gpuVals);
    return 0;
 
 }
