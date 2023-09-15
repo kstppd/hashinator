cc6842682f37ec13e69660c10b60f80ddcaf7231
kstppd <kpapadakis@protonmail.com>
2023-09-15 15:01:28 +0300

    Update tbPerf etst

--- unit_tests/benchmark/tbPerf.cu
+++ unit_tests/benchmark/tbPerf.cu
@@ -4,7 +4,7 @@
 #include <unordered_set>
 #include <random>
 #include "../../include/hashinator/hashinator.h"
-constexpr int R = 10;
+static constexpr int R = 10;
 
 using namespace std::chrono;
 using namespace Hashinator;
@@ -14,8 +14,6 @@ typedef split::SplitVector<hash_pair<key_type,val_type>> vector ;
 typedef split::SplitVector<key_type> key_vec;
 typedef split::SplitVector<val_type> val_vec;
 using hashmap= Hashmap<key_type,val_type>;
-
-
 
 auto generateNonDuplicatePairs(vector& src,const size_t size)->void {
     std::unordered_set<int> keys;
@@ -35,24 +33,6 @@ auto generateNonDuplicatePairs(vector& src,const size_t size)->void {
     }
 }
 
-auto generateNonDuplicatePairs(key_vec &keys,val_vec& vals,const size_t size)->void {
-    std::unordered_set<int> unique_keys;
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_int_distribution<val_type> dist(1, std::numeric_limits<val_type>::max());
-    keys.clear();
-    vals.clear();
-    while (keys.size() < size) {
-        val_type key = dist(gen);
-        // Check if the key is already present
-        if (unique_keys.find(key) == unique_keys.end()) {
-           val_type val=static_cast<val_type>(key/2);
-            keys.push_back(key);
-            vals.push_back(val);
-            unique_keys.insert(key);
-        }
-    }
-}
 
 template <class Fn, class ... Args>
 auto timeMe(Fn fn, Args && ... args){
@@ -61,79 +41,73 @@ auto timeMe(Fn fn, Args && ... args){
    start = std::chrono::high_resolution_clock::now();
    fn(args...);
    stop = std::chrono::high_resolution_clock::now();
-   auto duration = duration_cast<milliseconds>(stop- start).count();
+   auto duration = duration_cast<microseconds>(stop- start).count();
    total_time+=duration;
    return total_time;
 }
 
-void benchInsert(hashmap& hmap,key_type* gpuKeys, val_type* gpuVals,int sz,float deleteRatio){
-   hmap.insert(gpuKeys,gpuVals,1<<sz,1);
-   hmap.erase(gpuKeys,0.5*( 1<<sz ));
-   hmap.insert(gpuKeys,gpuVals,1<<sz,1);
-   //hmap.insert(gpuKeys,gpuVals,1<<sz,1);
-   hmap.stats();
-   //hmap.erase(gpuKeys,deleteRatio*(1<<sz));
-   //hmap.insert(gpuKeys,gpuVals,1<<sz);
-   return ;
+enum class METHOD{
+   NOOP,
+   REHASH,
+   CLEAN
+};
+
+void precondition(hashmap& hmap,vector& src,key_vec& keys, val_vec& vals,size_t size){
+   hmap.insert(src.data(),0.9*size,1);
+   hmap.erase(keys.data(),0.2*(size));
 }
 
-void benchInsert2(hashmap& hmap,vector& src,key_vec& keys, val_vec& vals,int sz,float deleteRatio){
-   hmap.insert(src.data(),1<<sz,1);
-   hmap.stats();
-   hmap.erase(keys.data(),deleteRatio*(1<<sz));
-   hmap.stats();
-   hmap.insert(src.data(),1<<sz,1);
-   hmap.stats();
-   return ;
-}
-
-int main(int argc, char* argv[]){
-
-   int sz= 14;
-   float deleteRatio = 0.5;
-   if (argc>=2){
-      deleteRatio = atof(argv[1]);
+void insert_control(hashmap& hmap,vector& src,key_vec& keys, val_vec& vals,size_t size,METHOD method){
+   switch (method){
+      case METHOD::NOOP:
+         break;
+      case METHOD::REHASH:
+         hmap.device_rehash(std::log2(size));
+         break;
+      case METHOD::CLEAN:
+         hmap.clean_tombstones();
+         break;
+      default:
+         assert(0 && "No method selected!");
    }
-   hashmap hmap(sz+1);
+   hmap.insert(src.data(),0.1*size,1);
+}
+
+double test(int sz,vector& cpu_src,key_vec& keyBuffer,val_vec& valBuffer,METHOD method){
+   hashmap hmap(sz);
    hmap.optimizeGPU();
-   vector cpu_src;
-   key_vec cpu_keys;
-   val_vec cpu_vals;
-   generateNonDuplicatePairs(cpu_keys,cpu_vals,1<<sz);
-   generateNonDuplicatePairs(cpu_src,1<<sz);
    cpu_src.optimizeGPU();
-   std::cout<<"Generated "<<cpu_keys.size()<<" unique keys!"<<std::endl;
-
-   key_type* gpuKeys;
-   val_type* gpuVals;
-   split_gpuMalloc((void **) &gpuKeys, (1<<sz)*sizeof(key_type));
-   split_gpuMalloc((void **) &gpuVals, (1<<sz)*sizeof(val_type));
-   split_gpuMemcpy(gpuKeys,cpu_keys.data(),(1<<sz)*sizeof(key_type),split_gpuMemcpyHostToDevice);
-   split_gpuMemcpy(gpuVals,cpu_vals.data(),(1<<sz)*sizeof(key_type),split_gpuMemcpyHostToDevice);
-
-   key_vec keyBuffer;
-   val_vec valBuffer;
-   for (auto& i : cpu_src){
-      keyBuffer.push_back(i.first);
-      valBuffer.push_back(i.first);
-   }
    keyBuffer.optimizeGPU();
    valBuffer.optimizeGPU();
-
    double t={0};
    for (int i =0; i<R; i++){
       hmap.optimizeGPU();
       keyBuffer.optimizeGPU();
       valBuffer.optimizeGPU();
       cpu_src.optimizeGPU();
-      //t+=timeMe(benchInsert,hmap,gpuKeys,gpuVals,sz,deleteRatio);
-       t+= timeMe(benchInsert2,hmap,cpu_src,keyBuffer,valBuffer,sz,deleteRatio);
+      precondition(hmap,cpu_src,keyBuffer,valBuffer,cpu_src.size());
+      t+=timeMe(insert_control,hmap,cpu_src,keyBuffer,valBuffer,cpu_src.size(),method);
       hmap.clear();
    }
-   std::cout<<"Done in "<<t/R<<" ms"<<std::endl;
+   return t/R;
+}
 
-   split_gpuFree(gpuKeys);
-   split_gpuFree(gpuVals);
+
+int main(){
+   printf("Results for Control Sizepower-- Device Rehash -- Tombstone Cleaning\n");
+   for (int sz=10; sz<=24;sz++){
+      vector cpu_src;
+      key_vec keyBuffer;
+      val_vec valBuffer;
+      generateNonDuplicatePairs(cpu_src,(1<<sz));
+      for (auto& i : cpu_src){
+         keyBuffer.push_back(i.first);
+         valBuffer.push_back(i.first);
+      }
+      auto time_control = test(sz,cpu_src,keyBuffer,valBuffer,METHOD::NOOP);
+      auto time_rehash = test(sz,cpu_src,keyBuffer,valBuffer,METHOD::REHASH);
+      auto time_clean = test(sz,cpu_src,keyBuffer,valBuffer,METHOD::CLEAN);
+      printf("%d \t %.03f %.f %.03f \n",sz,time_control,time_rehash,time_clean);
+   }
    return 0;
-
 }
